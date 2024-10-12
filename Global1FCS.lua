46ddb427e64421f042acb22274e8b761ff4e4a06
Inertia05 <92007270+Inertia05@users.noreply.github.com>
2024-10-12 07:23:38 -0500

    Add files via upload

--- Global1FCS.lua
+++ Global1FCS.lua
@@ -26,15 +26,18 @@ FCS_Running_Data = {
 
 
     --- Data related to the AOLSCS system (Allied Orbital Laser Strike Coordination System)
-    ---@class AOLSCS
+    ---@class AOLSCS:FCS
     AOLSCS = {
         --- Name of the FCS system
         ---@type string
         name = "AOLSCS",
 
         --- Dictionary to store allocated targets
-        ---@type table<number, boolean>
+        ---@type table<UnitID, boolean>
         target_allocated_dict = {},
+        --- Dictionary to store artillery that has been assigned to a target
+        --- @type table<UnitID, boolean>
+        artillery_allocated_dict = {},
 
         --- Checks if the target can be allocated (detected and not already allocated)
         ---@param self AOLSCS
@@ -44,12 +47,35 @@ FCS_Running_Data = {
             return not self:isTargetAllocated(target) and UnitSightedbyHumanPlayer_PVE_Slow(target)
         end,
 
+        --- Checks if the artillery can be allocated to the target
+        --- @param self AOLSCS
+        --- @param artillery StandardUnitType The artillery unit to be checked
+        --- @param target StandardUnitType The target to be checked
+        --- @return boolean True if the artillery can be allocated, false otherwise
+        canAllocateArtilleryToTarget = function(self, artillery, target)
+            -- TODO
+            return true
+            -- return not self:isArtilleryAllocated(artillery) and self:canAllocateTarget(target)
+        end,
+
         --- Allocates the target
         ---@param self AOLSCS
-        ---@param target any The target to allocate
-        allocateTarget = function(self, target)
+        ---@param artillery StandardUnitType The artillery unit to be allocated to the target
+        ---@param target StandardUnitType The target to be allocated to one or more artillery units
+        allocateArtilleryToTarget = function(self, artillery, target)
             local target_id = ObjectGetId(target)
+            if target_id == nil then
+                _ALERT("FCS_Running_Data.AOLSCS.allocateTarget: Invalid target")
+                return
+            end
+            local artillery_id = ObjectGetId(artillery)
+            if artillery_id == nil then
+                _ALERT("FCS_Running_Data.AOLSCS.allocateTarget: Invalid artillery")
+                return
+            end
             self.target_allocated_dict[target_id] = true
+            self.artillery_allocated_dict[artillery_id] = true
+            UnitAttackTarget(artillery, target)
         end,
 
         --- Checks if the target is already allocated
@@ -58,7 +84,24 @@ FCS_Running_Data = {
         ---@return boolean True if the target is allocated, false otherwise
         isTargetAllocated = function(self, target)
             local target_id = ObjectGetId(target)
+            if target_id == nil then
+                _ALERT("FCS_Running_Data.AOLSCS.isTargetAllocated: Invalid target")
+                return false
+            end
             return self.target_allocated_dict[target_id] ~= nil
+        end,
+
+        --- Checks if the artillery is already allocated to a target
+        --- @param self AOLSCS
+        --- @param artillery StandardUnitType The artillery unit to check
+        --- @return boolean True if the artillery is allocated, false otherwise
+        isArtilleryAllocated = function(self, artillery)
+            local artillery_id = ObjectGetId(artillery)
+            if artillery_id == nil then
+                _ALERT("FCS_Running_Data.AOLSCS.isArtilleryAllocated: Invalid artillery")
+                return false
+            end
+            return self.artillery_allocated_dict[artillery_id] ~= nil
         end
     },
 
@@ -106,12 +149,200 @@ FCS_Running_Data = {
     ---@param self FCS_Running_Data
     ---@param fcs string The FCS system to reset (e.g., "AOLSCS", "STBMFAS")
     resetTargetAllocation = function(self, fcs)
-        if self[fcs] and self[fcs].target_allocated_dict then
+        if self[fcs] then
             self[fcs].target_allocated_dict = {}
+            self[fcs].artillery_allocated_dict = {}
         else
             _ALERT("FCS_Running_Data.resetTargetAllocation: Invalid FCS = " .. fcs)
         end
-    end
+    end,
+
+    --- This function find and group nearby units
+    --- @param self FCS_Running_Data
+    --- @param current StandardUnitType
+    --- @param grouping_range_threshold number
+    --- @param artillery_grouped table a dictionary that record if an artillery is already grouped
+    --- @param player_index integer the index of the player
+    --- @param fcs string the name of the FCS
+    --- @param artillery_table UnitCollection the table of artillery
+    --- @return StandardUnitType[] the array of grouped units
+    --- @return integer the size of the array
+    _findAndGroupNearbyUnits = function (self, current, grouping_range_threshold, artillery_grouped, player_index, fcs, artillery_table)
+        local radius = grouping_range_threshold
+        local matchedObjects, count = Area_Friendly_Unit_filter_search(current, radius, artillery_table.filter_friendly)
+        local fcs_group = {}
+        local fcs_group_size = 0
+
+        if count > 1 then
+            for j = 1, count, 1 do
+                local current_artillery = matchedObjects[j]
+                if ObjectIsAlive(current_artillery) then
+                    local current_artillery_id = ObjectGetId(current_artillery)
+                    if current_artillery_id ~= nil then
+                        artillery_grouped[current_artillery_id] = true
+                        if ObjectStanceIsHoldPosition_Slow(current_artillery) then
+                            fcs_group_size = fcs_group_size + 1
+                            fcs_group[fcs_group_size] = current_artillery
+                            if FCS_Active_Display:isDisplayAllowed(player_index, fcs) then
+                                FCS_Active_Display:displayOnUnit(current_artillery, player_index, fcs)
+                            end
+                        end
+                    end
+                end
+            end
+        end
+
+        return fcs_group, fcs_group_size
+    end,
+
+    --- Function to search for targets and allocate to the artillery
+    --- @param self FCS_Running_Data
+    --- @param fcs_group StandardUnitType[] the array of grouped units
+    --- @param fcs_group_size integer the size of the array
+    --- @param artillery_range integer the range of the artillery
+    --- @param current StandardUnitType
+    --- @param player_index integer
+    --- @param fcs string
+    _searchAndAllocateTargets = function (self, fcs_group, fcs_group_size, artillery_range, current, player_index, fcs)
+        local group_radius = FCS_Running_Data:_calculate_FCS_Group_radius(fcs_group, fcs_group_size)
+        local guaranteed_radius = artillery_range - group_radius
+        local cx, cy, cz = FCS_Running_Data:_calculate_FCS_Group_center(fcs_group, fcs_group_size)
+        local matchedTargets, target_count = Area_enemy_search_surface_only(cx, cy, cz, guaranteed_radius, current)
+        --- @type FCS
+        local fcs_data = FCS_Running_Data[fcs]
+        local target_marked_count = 0
+        -- allocate one target in guaranteed_radius to each artillery
+        for artillery_index = 1, fcs_group_size, 1 do
+            local artillery = fcs_group[artillery_index]
+            local artillery_x, artillery_y, artillery_z = ObjectGetPosition(artillery)
+            for target_index = 1, target_count, 1 do
+                local target = matchedTargets[target_index]
+                if fcs_data:canAllocateTarget(target) then
+                    fcs_data:allocateArtilleryToTarget(artillery, target)
+                    target_marked_count = target_marked_count + 1
+                    break
+                end
+            end
+            if target_marked_count == target_count then
+                break
+            end
+        end
+
+        if target_marked_count < fcs_group_size then
+            -- Some units still need targets, search individually
+            for artillery_index = target_marked_count + 1, fcs_group_size, 1 do
+                local artillery = fcs_group[artillery_index]
+                local artillery_x, artillery_y, artillery_z = ObjectGetPosition(artillery)
+                local matchedTargets, target_count = Area_enemy_search_surface_only(artillery_x, artillery_y, artillery_z, artillery_range, current)
+                for target_index = 1, target_count, 1 do
+                    local target = matchedTargets[target_index]
+                    if fcs_data:canAllocateTarget(target) then
+                        fcs_data:allocateArtilleryToTarget(artillery, target)
+                        break
+                    end
+                end
+            end
+        end
+    end,
+
+    --- Group and allocate targets for the artillery
+    --- @param self FCS_Running_Data
+    --- @param artillery_table UnitCollection
+    --- @param player_index integer
+    --- @param artillery_grouping_range_threshold any
+    --- @param artillery_range any
+    --- @param fcs any
+    groupAndAllocateTargets = function (self, artillery_table, player_index, artillery_grouping_range_threshold, artillery_range, fcs)
+        local size = artillery_table[player_index].size
+        local rebuild_table = false
+        local artillery_grouped = {}
+        local fcs_data = FCS_Running_Data[fcs]
+        for i = 1, size, 1 do
+            local current = artillery_table[player_index][i]
+            if not ObjectIsAlive(current) then
+                artillery_table[player_index][i] = nil
+                rebuild_table = true
+            else
+                local artillery_id = ObjectGetId(current)
+                local is_in_group = artillery_grouped[artillery_id]
+                local is_allocated = FCS_Running_Data[fcs]:isArtilleryAllocated(current)
+                local current_target = ObjectFindTarget(current)
+
+                if (not is_allocated) and (not is_in_group) then
+                    local fcs_group, fcs_group_size = self:_findAndGroupNearbyUnits(current, 
+                    artillery_grouping_range_threshold, artillery_grouped, 
+                    player_index, fcs, artillery_table)
+                    
+                    if fcs_group_size > 1 then
+                        self:_searchAndAllocateTargets(fcs_group, fcs_group_size, artillery_range, current, player_index, fcs)
+                    else
+                        if ObjectStanceIsHoldPosition_Slow(current) then
+                            local x, y, z = ObjectGetPosition(current)
+                            local matchedTargets, target_count = Area_enemy_search_surface_only(x, y, z, artillery_range, current)
+                            for target_index = 1, target_count, 1 do
+                                local target = matchedTargets[target_index]
+                                if fcs_data:canAllocateTarget(target) then
+                                    fcs_data:allocateArtilleryToTarget(current, target)
+                                    if FCS_Active_Display:isDisplayAllowed(player_index, fcs) then
+                                        FCS_Active_Display:displayOnUnit(current, player_index, fcs)
+                                    end
+                                    break
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+
+        if rebuild_table then
+            _Rebuild_Table_with_Nils_Removed(artillery_table, player_index)
+        end
+    end,
+
+    --- Calculate the center of the group
+    --- Time complexity: O(n) where n is fcs_group_size
+    --- @param fcs_group table<number, StandardUnitType> The group of units
+    --- @param fcs_group_size number The size of the group
+    --- @return number, number, number The x, y, z coordinates of the group center
+    _calculate_FCS_Group_center = function(self, fcs_group, fcs_group_size)
+        local x = 0
+        local y = 0
+        local z = 0
+        for i = 1, fcs_group_size, 1 do
+        local cx, cy, cz = ObjectGetPosition(fcs_group[i])
+        x = x + cx
+        y = y + cy
+        z = z + cz
+        end
+        x = x / fcs_group_size
+        y = y / fcs_group_size
+        z = z / fcs_group_size
+        return x, y, z
+    end,
+  
+  
+    --- This function calculate the radius of the group
+    --- Time complexity: O(n) where n is fcs_group_size
+    --- @param fcs_group table<number, StandardUnitType> The group of units
+    --- @param fcs_group_size number The size of the group
+    --- @return number The radius of the group
+    _calculate_FCS_Group_radius = function(self, fcs_group, fcs_group_size)
+        local cx, cy, cz = self:_calculate_FCS_Group_center(fcs_group, fcs_group_size)
+        local group_radius = 0
+        for i = 1, fcs_group_size, 1 do
+        local unit = fcs_group[i]
+        local x, y, z = ObjectGetPosition(unit)
+        local distance_to_center_2D = sqrt((x - cx) * (x - cx) + (y - cy) * (y - cy))
+        if distance_to_center_2D > group_radius then
+            group_radius = distance_to_center_2D
+        end
+        end
+        return group_radius
+    end,
+  
+  
+
 }
 
 
@@ -207,24 +438,25 @@ FCS_Active_Display = {
 
 --- @type UnitCollection Athena_table
 Athena_table = {
-  [1] = {size = 0, time = {}},  -- Player 1
-  [2] = {size = 0, time = {}},  -- Player 2
-  [3] = {size = 0, time = {}},  -- Player 3
-  [4] = {size = 0, time = {}},  -- Player 4
-  [5] = {size = 0, time = {}},  -- Player 5
-  [6] = {size = 0, time = {}},  -- Player 6
-  filter_friendly = CreateObjectFilter({
+    [1] = {size = 0, time = {}, unit_id = {}, evac_dict = {}}, -- Player 1
+    [2] = {size = 0, time = {}, unit_id = {}, evac_dict = {}}, -- Player 2
+    [3] = {size = 0, time = {}, unit_id = {}, evac_dict = {}}, -- Player 3
+    [4] = {size = 0, time = {}, unit_id = {}, evac_dict = {}}, -- Player 4
+    [5] = {size = 0, time = {}, unit_id = {}, evac_dict = {}}, -- Player 5
+    [6] = {size = 0, time = {}, unit_id = {}, evac_dict = {}}, -- Player 6
+
+    filter_friendly = CreateObjectFilter({
     Rule="ANY",
     Relationship = "ALLIES",
     IncludeThing={
-      "AlliedAntiStructureVehicle",
+        "AlliedAntiStructureVehicle",
     },
-  }),
-  filter_neutral = CreateObjectFilter({
+    }),
+    filter_neutral = CreateObjectFilter({
     IncludeThing={
-      "AlliedAntiStructureVehicle",
+        "AlliedAntiStructureVehicle",
     },
-  }),
+    }),
 }
 
 
